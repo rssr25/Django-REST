01750c9971c7edb66f335bd1960ce5960c5f84fa
rahul <rahulsharmacs50@gmail.com>
2020-01-16 16:01:35 +0530

    practical api module

---
+++ src/updates/api/views.py
@@ -0,0 +1,33 @@
+
+
+from updates.models import Update as UpdateModel
+
+#creating, retrieving, updating, deleting (1) -- Update Model
+
+class UpdateModelDetailAPIView():
+
+	def get(self, request, *args, **kwargs):
+
+		return #json 
+
+
+	def post(self, request, *args, **kwargs):
+
+		return #json 
+
+
+	def put(self, request, *args, **kwargs):
+
+		return #json 
+
+
+	def delete(self, request, *args, **kwargs):
+
+		return #json 
+
+
+class UpdateModelListAPIView():
+
+	def get(self, request, *args, **kwargs):
+
+		return #json
