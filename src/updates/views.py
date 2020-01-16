ad16bcc9d1ce19727495149b5e31cdd44a6a0497
rahul <rahulsharmacs50@gmail.com>
2020-01-16 13:13:31 +0530

    example json response

--- src/updates/views.py
+++ src/updates/views.py
@@ -1,3 +1,22 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+#def detail_view(request):
+#	return render() # return JSON data or XML data. This is shortcut for HTTPResponse
+
+
+
+def json_example_view(request):
+
+	'''
+	URI -- URI for a REST API
+	GET -- retrieve
+	'''
+	data = {
+		"count":1000,
+		"content": "Some newer content"
+	}
+
+	return JsonResponse(data)
