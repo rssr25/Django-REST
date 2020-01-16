794d3531883d9a3837341f6070d6457f51251168
rahul <rahulsharmacs50@gmail.com>
2020-01-16 16:33:00 +0530

    practical api module part 2

--- src/cfeapi/urls.py
+++ src/cfeapi/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from updates.views import (
 			json_example_view, 
 			JsonCBV, 
@@ -25,10 +25,11 @@ from updates.views import (
 
 
 urlpatterns = [
-    path('admin/' 					, admin.site.urls),
-    path('json/example/'			, json_example_view),
-    path('json/cbv/'				, JsonCBV.as_view()),
-    path('json/cbv2/'				, JsonCBV2.as_view()),
-    path('json/serialized/list/'	, SerializedListView.as_view()),
-    path('json/serialized/detail/'	, SerializedDetailView.as_view())
+    path('admin/' 			, admin.site.urls),
+    path('api/updates/'		, include('updates.api.urls')) #api/updates (list) api/updates/1/ --> detail
+    # path('json/example/'			, json_example_view),
+    # path('json/cbv/'				, JsonCBV.as_view()),
+    # path('json/cbv2/'				, JsonCBV2.as_view()),
+    # path('json/serialized/list/'	, SerializedListView.as_view()),
+    # path('json/serialized/detail/'	, SerializedDetailView.as_view())
 ]
--- src/updates/api/views.py
+++ src/updates/api/views.py
@@ -1,33 +1,50 @@
-
-
+from django.views.generic import View
+from django.http import HttpResponse
 from updates.models import Update as UpdateModel
 
 #creating, retrieving, updating, deleting (1) -- Update Model
 
-class UpdateModelDetailAPIView():
+class UpdateModelDetailAPIView(View):
 
-	def get(self, request, *args, **kwargs):
+	'''
+	Retrieve, Update, Delete --> Object
+	'''
+	def get(self, request, id, *args, **kwargs):
 
-		return #json 
+		obj = UpdateModel.objects.get(id=id)
+		json_data = obj.serialize()
+
+		return HttpResponse(json_data, content_type='application/json')
 
 
 	def post(self, request, *args, **kwargs):
 
-		return #json 
+		return HttpResponse({}, content_type='application/json')  
 
 
 	def put(self, request, *args, **kwargs):
 
-		return #json 
-
+		return HttpResponse({}, content_type='application/json')
 
 	def delete(self, request, *args, **kwargs):
 
-		return #json 
+		return HttpResponse({}, content_type='application/json') 
 
 
-class UpdateModelListAPIView():
+class UpdateModelListAPIView(View):
 
+	'''
+	List View
+	Create View
+
+	'''
 	def get(self, request, *args, **kwargs):
 
-		return #json
+		qs = UpdateModel.objects.all()
+		json_data = qs.serialize()
+
+		return HttpResponse(json_data, content_type='application/json')
+
+	def post(self, request, *args, **kwargs):
+
+		return HttpResponse({}, content_type='application/json')  
