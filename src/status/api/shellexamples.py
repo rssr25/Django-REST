9f828767c56532b3e994ebeaaed0d3f644cbe8c1
Rahul Sharma <rahulsharmacs50@gmail.com>
2020-01-21 14:13:49 +0530

    create and update through serizlizers : DRF

--- src/status/api/shellexamples.py
+++ src/status/api/shellexamples.py
@@ -33,3 +33,41 @@ print(json_data)
 stream2 = BytesIO(json_data2)
 data2 = JSONParser().parse(stream2)
 print(data2)
+
+
+'''
+Create Obj
+'''
+data = {'user':1}
+serializer = StatusSerializer(data = data)
+serializer.is_valid()
+serializer.save()
+
+
+
+
+'''
+Update Obj
+'''
+data = {'content':'some new content', 'user':1}
+update_serializer = StatusSerializer(obj, data=data)
+update_serializer.is_valid()
+update_serializer.save()
+
+
+
+'''
+Delete Obj
+'''
+data = {'content':'please delete me', 'user':1}
+create_obj_serializer = StatusSerializer(data=data)
+create_obj_serializer.is_valid()
+create_obj = create_obj_serializer.save()
+print(create_obj)
+
+#data = {"id":6}
+obj = Status.objects.last()
+get_data_serializer = StatusSerializer(obj)
+# update_serializer.is_valid()
+# update_serializer.save()
+print(get_data_serializer)
