88ace91dd2d1f71781972b417ff796d6ab7e4349
Rahul Sharma <rahulsharmacs50@gmail.com>
2020-01-21 13:27:26 +0530

    DRF-model form for validation

--- src/status/admin.py
+++ src/status/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Status
+from .forms import StatusForm 
+
+
+class StatusAdmin(admin.ModelAdmin):
+	list_display = ['user', '__str__', 'image']
+	form = StatusForm
+	# class Meta:
+	# 	model = Status
 
 # Register your models here.
-admin.site.register(Status)
+admin.site.register(Status, StatusAdmin)
--- src/status/models.py
+++ src/status/models.py
@@ -24,3 +24,8 @@ class Status(models.Model): #fb status, instagram post, tweet, linkedin post
 
 	def __str__(self):
 		return str(self.content)[:50]
+
+
+	class Meta:
+		verbose_name = 'Status post'
+		verbose_name_plural = "Status posts"
