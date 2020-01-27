acb70a9d53eed82cb47a9bc1360e66021772a51e
Rahul Sharma <rahulsharmacs50@gmail.com>
2020-01-27 15:53:55 +0530

    Validation adn Fields : DRF

--- src/status/api/serializers.py
+++ src/status/api/serializers.py
@@ -18,3 +18,21 @@ class StatusSerializer(serializers.ModelSerializer):
 			'content',
 			'image'
 		]
+
+
+	def validate_content(self, value):
+		if len(value) > 10000:
+			raise serializers.ValidationError("This is way too long!")
+
+		return value
+
+
+	def validate(self, data):
+		content = data.get("content", None)
+		if content == "":
+			content = None
+
+		image = data.get("image", None)
+		if content is None and image is None:
+			raise serializers.ValidationError("content or image is required")
+		return data
