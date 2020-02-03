8c1ec7b7598011f19ab581a44d9cd17a3f0bffdc
Rahul Sharma <rahulsharmacs50@gmail.com>
2020-02-03 12:31:18 +0530

    API endpoints overview: DRF

---
+++ src/status/api/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    
+     path('', StatusListSearchAPIView.as_view()),                    #/api/status -> List
+     path('create/'			  , StatusCreateAPIView.as_view()),      #/api/status/create -> Create
+     path('<int:id>/'		  , StatusDetailAPIView.as_view()),      #/api/status/12/ -> Detail
+     path('<int:id>/update/'  , StatusUpdateAPIView.as_view()),      #/api/status/12/update -> Update
+     path('<int:id>/delete/'  , StatusDeleteAPIView.as_view()),      #/api/status/12/delete -> Delete
+
+]
+
+
+### START WITH ####
+# /api/status -> List
+# /api/status/create -> Create
+# /api/status/12/ -> Detail
+# /api/status/12/update -> Update
+# /api/status/12/delete -> Delete
+
+
+###END WITH####
+
+# /api/status/ -> List (ALL CRUD)
+# /api/status/ -> Detail (ALL CRUD)
