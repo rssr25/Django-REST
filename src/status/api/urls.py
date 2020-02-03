f61f65f6bbafc9177e7281da74f94906d9fa48a2
Rahul Sharma <rahulsharmacs50@gmail.com>
2020-02-03 13:55:03 +0530

    Detail API View : DRF

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import StatusAPIView, StatusCreateAPIView
+from .views import (
+    StatusAPIView, 
+    StatusCreateAPIView, 
+    StatusDetailAPIView
+    )
 
 
 urlpatterns = [
     
      path('', StatusAPIView.as_view()),                    #/api/status -> List
      path('create/'			  , StatusCreateAPIView.as_view()),      #/api/status/create -> Create
-     #path('<int:id>/'		  , StatusDetailAPIView.as_view()),      #/api/status/12/ -> Detail
+     path('<int:id>/'		  , StatusDetailAPIView.as_view()),      #/api/status/12/ -> Detail
      #path('<int:id>/update/'  , StatusUpdateAPIView.as_view()),      #/api/status/12/update -> Update
      #path('<int:id>/delete/'  , StatusDeleteAPIView.as_view()),      #/api/status/12/delete -> Delete
 
